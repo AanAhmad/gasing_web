a6293acf23c1e46db3176ca53c970e91681bb5b3
AanAhmad <rikalistianalava@gmail.com>
2023-10-23 16:33:12 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
-let wrap = document.getElementById("wrap");
-let xp_btn = document.getElementById("exp-btn");
-let sub = document.getElementById("sub");
+document.addEventListener("DOMContentLoaded", function () {
+  const back = document.getElementById("backB");
+  const front = document.getElementById("frontB");
+  const titleB = document.getElementById("titleB");
+  const titleBm = document.getElementById("titleBm");
+  const xp_btn = document.getElementById("xp-btn");
+  const sub = document.getElementById("sub");
+  const descB = document.getElementById("descB");
 
-wrap.addEventListener("scroll", function () {
-  let value = this.scrollTop;
-  let roundvalue = Math.round(value);
-  xp_btn.style.transform = "translateY(" + roundvalue * 0.3 + "px)";
-  sub.style.transform = "translateY(" + roundvalue * 0.3 + "px)";
+  window.addEventListener("scroll", function () {
+    let value = this.scrollY;
+    console.log(value);
+    front.style.transform = "translateY(" + value * 0.2 + "px)";
+    back.style.transform = "translateY(" + value * 0.3 + "px)";
+    titleB.style.transform = "translateY(" + value * 0.3 + "px)";
+    titleBm.style.transform = "translateY(" + value * 0.3 + "px)";
+    xp_btn.style.transform = "translateY(" + value * 0.3 + "px)";
+    sub.style.transform = "translateY(" + value * 0.3 + "px)";
+    descB.style.transform = "translateY(" + value * 0.3 + "px)";
+  });
 });
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("slideDown");
+function checkScroll() {
+  var elements = document.querySelectorAll("section");
+
+  elements.forEach(function (element) {
+    let top = window.scrollY;
+    let offset = element.offsetTop - 470;
+    let height = element.offsetHeight;
+    if (top >= offset && top <= offset + height) {
+      element.classList.add("show-animate");
     } else {
-      entry.target.classList.remove("slideDown");
+      element.classList.remove("show-animate");
     }
   });
+}
+
+window.addEventListener("load", function () {
+  checkScroll();
+});
+
+window.addEventListener("scroll", function () {
+  checkScroll();
 });
